4a2363e5cf0fcdca2891986168283f45959380db
BuildTools <trenamanluke@gmail.com>
2020-03-29 13:30:28 -0400

    Rebalanced difficulty on master, without API changes

--- snakemaze/src/components/buildSnake.js
+++ snakemaze/src/components/buildSnake.js
@@ -404,8 +404,7 @@ export default class{
                 return !(val.x === spawn.x && val.y === spawn.y);
             }) && a.gems.every(function(val) {
                     return !(val.coord.x === spawn.x && val.coord.y === spawn.y);
-                })
-             && g.maze.data[spawn.y][spawn.x] === 2;
+                });
             if (safe) {
                 let gem = new PIXI.Sprite(PIXI.loader.resources["assets/gem-" + Math.ceil(Math.random() * 3) + ".png"].texture)
                 gem.x = spawn.x * 32;
