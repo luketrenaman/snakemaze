04ca243c73b24e8fd1b518891576aaa0a7c4dbcf
luketrenaman <trenamanluke@gmail.com>
2021-09-01 15:16:18 -0400

    Recover gulpfile and remove extra Tiled files

--- tile.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="tile" tilewidth="32" tileheight="32" tilecount="342" columns="18">
- <image source="../rotator/game.png" width="602" height="632"/>
-</tileset>
